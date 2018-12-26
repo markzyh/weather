8f3ed1c0939b0b96fe7816745bcfe261939c0e1c
markzyh <980511383@qq.com>
2018-12-26 17:07:22 +0800

    refs

--- src/components/pages/index/index.vue
+++ src/components/pages/index/index.vue
@@ -14,9 +14,14 @@
       <!-- <ul class="">
         <li></li>
       </ul>-->
-      <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback" class="forecast_list">
+      <swiper
+        :options="swiperOption"
+        ref="mySwiper"
+        @someSwiperEvent="callback"
+        class="forecast_list"
+      >
         <!-- slides -->
-        <swiper-slide class="weather_details" v-for="(item,index) in forecastWeather" :key="index">
+        <swiper-slide class="weather_details" v-for="(item,index) in forecastWeather" :key="index" ref='slider'>
           <h3 class="day">今天</h3>
           <h4 class="date">{{item.date}}</h4>
           <p class="day_status">{{item.cond_txt_d}}</p>
@@ -27,21 +32,29 @@
           <p class="wind_sc">{{item.wind_sc}}级</p>
           <p class="air_qlty">良</p>
         </swiper-slide>
+        <canvas id="canvas" ref="canvas" class="tem_canvas"></canvas>
       </swiper>
     </div>
   </div>
 </template>
 <script>
 //import {_getNowWeather} from '@/api/ajax'
-import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
-  components:{
-    swiper, swiperSlide
+  components: {
+    swiper,
+    swiperSlide
   },
   name: "IndexPage",
   data() {
     return {
+      diff: 16,
+      HEIGHT: 200, //画布高度
+      //ONE_HEIGHT = HEIGHT / diff / 2,
+      WIDTH: 80,
+      RADIUS: 3,
+      PADDING: 10,
       today: "", //当天
       city: "上海", //本地市
       area: "宝山", //本地区
@@ -52,7 +65,9 @@ export default {
       nowWindSc: "", //风力
       nowTmp: "", //温度
       updateTime: "", //更新时间
-      week :[
+      highTemp: [],
+      lowTemp: [],
+      week: [
         "星期日",
         "星期一",
         "星期二",
@@ -62,14 +77,57 @@ export default {
         "星期六"
       ],
       swiperOption: {
-        slidesPerView :4
+        slidesPerView: 4
       }
     };
   },
   methods: {
-    callback(){},
+    //温度折线图
+    canvas() {
+      c.beginPath(arr, color);
+      //画点
+      for (let i = 0; i < arr.length; i++) {
+        c.moveTo(
+          this.WIDTH * i + this.PADDING,
+          -arr[i] * this.ONE_HEIGHT + this.HEIGHT
+        );
+        c.fillText(
+          arr[i] + "℃",
+          this.WIDTH * i,
+          -arr[i] * this.ONE_HEIGHT + this.HEIGHT - this.PADDING
+        );
+        c.arc(
+          this.WIDTH * i + this.PADDING,
+          -arr[i] * this.ONE_HEIGHT + this.HEIGHT,
+          this.RADIUS,
+          0,
+          2 * Math.PI,
+          true
+        );
+      }
+      c.fillStyle = color;
+      c.fill();
+      //划线
+      for (let j = 0; j <= arr.length; j++) {
+        c.moveTo(
+          this.WIDTH * j + this.PADDING,
+          -arr[j] * this.ONE_HEIGHT + HEIGHT
+        );
+        c.lineTo(
+          this.WIDTH * (j + 1) + this.PADDING,
+          -arr[j + 1] * this.ONE_HEIGHT + this.HEIGHT
+        );
+      }
+      c.strokeStyle = color;
+      c.stroke();
+    },
+    callback() {},
     handleForecastWeather() {
-      //this.forecastWeather = ''
+      let length = this.forecastWeather.length;
+      for (let i = 0; i < length; i++) {
+        this.highTemp.push(this.forecastWeather[i].tmp_max)
+        this.lowTemp.push(this.forecastWeather[i].tmp_min)
+      }
     },
     handleNowWeather() {
       if (this.nowweather.length != 0) {
@@ -128,6 +186,14 @@ export default {
     this.getToday();
     this.getNowWeather(this.area, this.city);
     this.getForecastWeather(this.area, this.city);
+    let canvas = document.getElementById("canvas");
+    //let c = canvas.getContext("2d");
+    setTimeout(() =>{
+    console.log(this.$refs.slider[0])
+
+    },2000)
+    console.log(canvas.offsetWidth)
+
     //this.swiper.slideTo(3, 1000, false)
   }
 };
@@ -135,15 +201,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss'>
-.weather_details{
-  h3,h4{
+.tem_canvas {
+  width: 400px;
+  height: 200px;
+  border: 1px solid #000;
+  position: absolute;
+  top: 280px;
+}
+.weather_details {
+  h3,
+  h4 {
     font-size: 26px;
     font-weight: normal;
     margin-top: 20px;
   }
-  p{
+  p {
     font-size: 20px;
     margin-top: 20px;
+  }
+  .night_status {
+    margin-top: 300px;
   }
 }
 .forecast_title {
@@ -153,8 +230,9 @@ export default {
 }
 .forecast_list {
   display: flex;
-  background: rgba(0,0,0,0.5);
-  padding:30px 0;
+  background: rgba(0, 0, 0, 0.5);
+  padding: 30px 0;
+  position: relative;
 }
 .now_tmp {
   font-size: 60px;
